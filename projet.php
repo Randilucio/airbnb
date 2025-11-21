a67552b6c7801bab9ebb3659a5d2915fef1f89c2
Randi Lucio <raitsarevolucio@gmail.com>
2025-11-21 10:56:35 +0100

    db

--- projet.php
+++ projet.php
@@ -1,3 +1,22 @@
 <?php
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname=airbnb;charset=utf8','root','');
+} catch(PDOException $e) {
+    die($e->getMessage());
+}
+
+$parPage = 10;
+$page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+$depart = ($page - 1) * $parPage;
+
+$sth = $dbh->prepare("SELECT * FROM `listing` LIMIT :start, :limit");
+$sth->bindValue(':start', $depart, PDO::PARAM_INT);
+$sth->bindValue(':limit', $parPage, PDO::PARAM_INT);
+$sth->execute();
+
+$resultats = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+$total = $dbh->query("SELECT COUNT(*) FROM `100`")->fetchColumn();
+$nbPages = ceil($total / $parPage);
 
 ?>
